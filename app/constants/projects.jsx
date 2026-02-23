55187cb43fd6688ad0077b6b4c16e1773fb7c07a
mur4t_s4hin <sahin.mur4t@gmail.com>
2026-02-24 01:31:21 +0300

    Matchscore.png and image import was mistakenly deleted. Added again.

--- app/constants/projects.jsx
+++ app/constants/projects.jsx
@@ -1,7 +1,10 @@
+import MatchScoreAppImage from '../../assets/matchscore.png'
+
 const projects = [
         { 
-            title: "Murat Sahin Dev Portfolio", 
+            title: "Match Score App", 
             description: "A mobile application to follow your favourite sports scores and fixtures in real-time.", 
+            imageUrl: MatchScoreAppImage,
             detailsLink: "https://apps.apple.com/tr/app/match-scores/id6752309800?l=tr"
         },
     ];
